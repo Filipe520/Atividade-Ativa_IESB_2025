684e66f68e646add689c4979bbb1988ed312f245
Filipe520 <filipealves520@gmail.com>
2025-04-27 19:52:04 -0300

    Finalizado main e footer

---
+++ src/app/components/footerLogin/index.tsx
@@ -0,0 +1,58 @@
+// Lib Icons
+import { FaFacebook, FaInstagram, FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+
+import Link from "next/link";
+
+// Styles css
+import styles from "./style.module.css";
+
+export default function FooterLogin() {
+  return (
+    <div className={styles.footer}>
+      <ul>
+        <li>
+          <Link
+            href="https://www.facebook.com/FilipeA520/"
+            target="_blank"
+            rel="external"
+          >
+            <FaFacebook />
+          </Link>
+          <Link
+            href="https://www.instagram.com/filipealves520/"
+            target="_blank"
+            rel="external"
+          >
+            <FaInstagram />
+          </Link>
+          <Link
+            href="https://github.com/Filipe520"
+            target="_blank"
+            rel="external"
+          >
+            <FaGithub />
+          </Link>
+          <Link
+            href="https://x.com/FilipeAlve89789"
+            target="_blank"
+            rel="external"
+          >
+            <FaSquareXTwitter />
+          </Link>
+        </li>
+      </ul>
+
+      <p>
+        Copyright &copy; 2025{" "}
+        <Link
+          href="https://github.com/Filipe520"
+          target="_blank"
+          rel="external"
+        >
+          Filipe Alves
+        </Link>
+      </p>
+    </div>
+  );
+}

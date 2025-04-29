4f72894d806fbf48fc59de1e2e559924497b9982
Filipe520 <filipealves520@gmail.com>
2025-04-29 17:52:26 -0300

    Entrando na parte de formulário

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import LoginScreen from "./pages/login/page";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <LoginScreen>{children}</LoginScreen>
       </body>
     </html>
   );
---
+++ src/app/pages/login/formLogin/page.tsx
@@ -0,0 +1,7 @@
+export default function FormLogin() {
+  return (
+    <div>
+      <h1>Formulário</h1>
+    </div>
+  );
+}

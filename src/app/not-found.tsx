04405856bdfce03f0fdac2e90b75b4d508cd8037
Filipe520 <filipealves520@gmail.com>
2025-11-22 23:29:12 -0300

    update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,18 @@
-"use client";
+"use client"; // obrigatório
 
 import Link from "next/link";
-import { GoArrowLeft } from "react-icons/go";
+import dynamic from "next/dynamic";
+
+const GoArrowLeft = dynamic(
+  () => import("react-icons/go").then((mod) => mod.GoArrowLeft),
+  { ssr: false }
+);
 
 export default function NotFound() {
   return (
-    <article className="flex max-md:flex-col justify-center items-center mx-auto  bg-white min-[500px]:h-[900px] h-dvh min-sm:py-5">
-      <section className="bg-white p-10 md:rounded-l-3xl  max-md:rounded-3xl flex flex-col md:w-xl justify-evenly h-full min-sm:border border-black/20 min-sm:ml-5">
-        <div className="flex flex-col gap-5 ">
+    <article className="flex max-md:flex-col justify-center items-center mx-auto bg-white min-[500px]:h-[900px] h-dvh min-sm:py-5">
+      <section className="bg-white p-10 md:rounded-l-3xl max-md:rounded-3xl flex flex-col md:w-xl justify-evenly h-full min-sm:border border-black/20 min-sm:ml-5">
+        <div className="flex flex-col gap-5">
           <p className="text-indigo-600">404</p>
           <h1 className="md:text-5xl max-md:text-4xl font-black">
             Página não encontrada
@@ -16,7 +21,6 @@ export default function NotFound() {
             Desculpe, não conseguimos encontrar a página que você está
             procurando.
           </p>
-
           <div>
             <Link
               href="/"
@@ -28,7 +32,7 @@ export default function NotFound() {
           </div>
         </div>
       </section>
-      <section className="bg-[url('/imagens/not-found-2.jpg')] bg-center bg-cover bg-no-repeat hidden max-md:hidden md:flex justify-start items-center rounded-r-3xl my-5 md:w-xl h-full mr-5"></section>
+      <section className="bg-[url('/imagens/not-found-2.jpg')] bg-center bg-cover bg-no-repeat hidden max-md:hidden md:flex justify-start items-center rounded-r-3xl my-5 md:w-xl h-full mr-5" />
     </article>
   );
 }
